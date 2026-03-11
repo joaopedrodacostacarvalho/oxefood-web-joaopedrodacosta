e90ce89a9200851a174ba706fae4afae034460c5
joaopedrodacostacarvalho <jpcc.dev@gmail.com>
2026-03-11 20:02:45 -0300

    fix list client

--- src/views/cliente/ListClient.jsx
+++ src/views/cliente/ListClient.jsx
@@ -1,19 +1,34 @@
-import axios from 'axios';
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table, Modal, Header } from 'semantic-ui-react';
-import MenuSistema from '../../MenuSistema';
+import { Button, Container, Divider, Header, Icon, Modal, Table } from "semantic-ui-react";
+import MenuSistema from "../../MenuSistema";
 
 export default function ListCliente() {
-
     const [lista, setLista] = useState([]);
     const [openModal, setOpenModal] = useState(false);
     const [idRemover, setIdRemover] = useState();
+    useEffect(() => {
+        carregarLista();
+    }, []);
 
+    function carregarLista() {
+        axios.get("http://localhost:8080/api/cliente").then((response) => {
+            setLista(response.data);
+        });
+    }
+    function formatarData(dataParam) {
+        if (dataParam === null || dataParam === "" || dataParam === undefined) {
+            return "";
+        }
+
+        let arrayData = dataParam.split("-");
+        return arrayData[2] + "/" + arrayData[1] + "/" + arrayData[0];
+    }
 
     function confirmaRemover(id) {
-        setOpenModal(true)
-        setIdRemover(id)
+        setOpenModal(true);
+        setIdRemover(id);
     }
 
     async function remover() {
@@ -35,52 +50,30 @@ export default function ListCliente() {
     }
 
 
-
-
-    useEffect(() => {
-        carregarLista();
-    }, [])
-
-    function carregarLista() {
-
-        axios.get("http://localhost:8080/api/cliente")
-            .then((response) => {
-                setLista(response.data)
-            })
-    }
-    function formatarData(dataParam) {
-
-        if (dataParam === null || dataParam === '' || dataParam === undefined) {
-            return ''
-        }
-
-        let arrayData = dataParam.split('-');
-        return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
-    }
     return (
         <div>
-            <MenuSistema tela={'cliente'} />
-            <div style={{ marginTop: '3%' }}>
-
-                <Container textAlign='justified' >
-
+            <MenuSistema tela={"cliente"} />
+            <div style={{ marginTop: "3%" }}>
+                <Container textAlign="justified">
                     <h2> Cliente </h2>
                     <Divider />
 
-                    <div style={{ marginTop: '4%' }}>
+                    <div style={{ marginTop: "4%" }}>
                         <Button
-                            label='Novo'
+                            label="Novo"
                             circular
-                            color='orange'
-                            icon='clipboard outline'
-                            floated='right'
+                            color="orange"
+                            icon="clipboard outline"
+                            floated="right"
                             as={Link}
-                            to='/form-cliente'
+                            to="/form-cliente"
                         />
-                        <br /><br /><br />
 
-                        <Table color='orange' sortable celled>
+                        <br />
+                        <br />
+                        <br />
 
+                        <Table color="orange" sortable celled>
                             <Table.Header>
                                 <Table.Row>
                                     <Table.HeaderCell>Nome</Table.HeaderCell>
@@ -88,51 +81,56 @@ export default function ListCliente() {
                                     <Table.HeaderCell>Data de Nascimento</Table.HeaderCell>
                                     <Table.HeaderCell>Fone Celular</Table.HeaderCell>
                                     <Table.HeaderCell>Fone Fixo</Table.HeaderCell>
-                                    <Table.HeaderCell textAlign='center'>Ações</Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="center">Ações</Table.HeaderCell>
                                 </Table.Row>
                             </Table.Header>
 
                             <Table.Body>
-
-                                {lista.map(cliente => (
-
+                                {lista.map((cliente) => (
                                     <Table.Row key={cliente.id}>
                                         <Table.Cell>{cliente.nome}</Table.Cell>
                                         <Table.Cell>{cliente.cpf}</Table.Cell>
-                                        <Table.Cell>{formatarData(cliente.dataNascimento)}</Table.Cell>
+                                        <Table.Cell>
+                                            {formatarData(cliente.dataNascimento)}
+                                        </Table.Cell>
                                         <Table.Cell>{cliente.foneCelular}</Table.Cell>
                                         <Table.Cell>{cliente.foneFixo}</Table.Cell>
-                                        <Table.Cell textAlign='center'>
-
+                                        <Table.Cell textAlign="center">
                                             <Button
                                                 inverted
                                                 circular
-                                                color='green'
-                                                title='Clique aqui para editar os dados deste cliente'
-                                                icon>
-                                                <Icon name='edit' />
-                                            </Button> &nbsp;
-                                            <Button
-                                                inverted
-                                                circular
-                                                color='red'
-                                                title='Clique aqui para remover este cliente'
+                                                color="green"
+                                                title="Clique aqui para editar os dados deste cliente"
                                                 icon
-                                                onClick={e => confirmaRemover(cliente.id)}>
-
-                                                <Icon name='trash' />
+                                            >
+                                                <Link
+                                                    to="/form-cliente"
+                                                    state={{ id: cliente.id }}
+                                                    style={{ color: "green" }}
+                                                >
+                                                    {" "}
+                                                    <Icon name="edit" />{" "}
+                                                </Link>
+                                            </Button>{" "}
+                                            &nbsp;
+                                            <Button
+                                                inverted
+                                                circular
+                                                color="red"
+                                                title="Clique aqui para remover este cliente"
+                                                icon
+                                                onClick={(e) => confirmaRemover(cliente.id)}
+                                            >
+                                                <Icon name="trash" />
                                             </Button>
-
                                         </Table.Cell>
                                     </Table.Row>
                                 ))}
-
                             </Table.Body>
                         </Table>
                     </div>
                 </Container>
             </div>
-
             <Modal
                 basic
                 onClose={() => setOpenModal(false)}
@@ -140,20 +138,26 @@ export default function ListCliente() {
                 open={openModal}
             >
                 <Header icon>
-                    <Icon name='trash' />
-                    <div style={{ marginTop: '5%' }}> Tem certeza que deseja remover esse registro? </div>
+                    <Icon name="trash" />
+                    <div style={{ marginTop: "5%" }}>
+                        {" "}
+                        Tem certeza que deseja remover esse registro?{" "}
+                    </div>
                 </Header>
                 <Modal.Actions>
-                    <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
-                        <Icon name='remove' /> Não
+                    <Button
+                        basic
+                        color="red"
+                        inverted
+                        onClick={() => setOpenModal(false)}
+                    >
+                        <Icon name="remove" /> Não
                     </Button>
-                    <Button color='green' inverted onClick={() => remover()}>
-                        <Icon name='checkmark' /> Sim
+                    <Button color="green" inverted onClick={() => remover()}>
+                        <Icon name="checkmark" /> Sim
                     </Button>
                 </Modal.Actions>
             </Modal>
-
-
         </div>
-    )
+    );
 }
